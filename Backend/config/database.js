7dffc077120973c15eca97c2b803dd089b3f5840
Ishimwe Chloe <karlychloee12@gmail.com>
2025-09-29 12:31:28 +0200

    Update database.js

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 
-    user: process.env.DB_USER || 
-    password: process.env.DB_PASSWORD || 
-    database: process.env.DB_NAME || 
-    port: process.env.DB_PORT || 27117,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
     ssl: {
         ca: fs.readFileSync(process.env.DB_CA_PATH),
         rejectUnauthorized: true 
