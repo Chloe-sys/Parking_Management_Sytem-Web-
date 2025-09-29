387e4b9110196e6be149fae5f6da103d037491b6
Ishimwe Chloe <karlychloee12@gmail.com>
2025-09-29 12:08:33 +0200

    Update database.js

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,10 +2,14 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'chloe',
-    password: process.env.DB_PASSWORD || '12345',
-    database: process.env.DB_NAME || 'parking_management',
+    host: process.env.DB_HOST || 'mysql-31725d0d-parking-management-system.k.aivencloud.com',
+    user: process.env.DB_USER || 'avnadmin',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'defaultdb',
+    port: process.env.DB_PORT || 27117,
+    ssl: {
+        rejectUnauthorized: true 
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
